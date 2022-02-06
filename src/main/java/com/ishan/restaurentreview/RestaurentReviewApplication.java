dffd6fce9f767c7f95f0d7ddb37f14516a6ac23b
Ishan Shah <imshah96@gmail.com>
2022-02-07 00:38:19 +0530

    Initial Project Setup

---
+++ src/main/java/com/ishan/restaurentreview/RestaurentReviewApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.restaurentreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurentReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurentReviewApplication.class, args);
+	}
+
+}
